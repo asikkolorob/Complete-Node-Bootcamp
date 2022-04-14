288e15f6e07b55ce49916c9fc8dbcf65b8bf79fe
AshikSovware <asikrahman4545@gmail.com>
2022-04-14 15:55:38 +0600

    basic node program

---
+++ index.js
@@ -0,0 +1,18 @@
+// const _lodash = require('lodash'); //? packege import
+// console.log(_lodash.last([12, 23, 23, 45, 56, 78])); //? lodash uses
+// console.log(global); //? global similar window
+
+//? This funcion come from global object
+// setTimeout(() => {
+//     console.log('time out funcion')
+// }, 2000);
+
+// console.log(__dirname); //? Directory Name identify
+// console.log(__filename); //? File Name Identify
+
+const people = require('./people');
+
+console.log(people);
+
+
+
---
+++ people.js
@@ -0,0 +1,9 @@
+var people = ['Rashed', 'Abdullah', 'Ashik', 'Rahat'];
+var num1 = 2;
+
+function test() {
+    console.log
+}
+
+module.exports =  people;
+
